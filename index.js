4f9cd7947164b94ce6ad9cf0dd52c6d4a0259808
João Pedro <118128987+JoaoPedro0116@users.noreply.github.com>
2023-08-17 14:32:18 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+// Esta função é chamada quando um botão numérico ou de operador é clicado na calculadora.
+function insert(num)
+{
+    var numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + num;
+
+// Essa função é chamada quando o botão "limpar" é clicado. 
+
+// Essa função é associada ao botão "voltar" e remove o último caractere da entrada da calculadora.
+//  Ela recupera o conteúdo do elemento HTML com o ID 'resultado', que deve conter uma expressão matemática
+
+
+
+}
+function back()
+{
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+}
+function calcular()
+{
+    var resultado = document.getElementById('resultado').innerHTML;
+    if(resultado)
+    {
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }
+    else
+    {
+        document.getElementById('resultado').innerHTML = "sem operações."
+    }
+}
+function clean()
+{
+    document.getElementById('resultado').innerHTML = "";
+}
